29eafefcca5576427b9bbab72637056a07e5248b
Nebojša Vasiljević <nebojsa.vasiljevic@gmail.com>
2018-08-18 17:29:06 +0200

    kopiranje slika kod buildovanja HTML-a

--- tasks.py
+++ tasks.py
@@ -1,15 +1,33 @@
 # koristi se invoke komanda za pokretanje taskova (vidi http://pyinvoke.org/, instalira se pip-om)
 
 import os
+import shutil
+
 from invoke import task
+
+def remove_files_from_dir(dir):
+    for f in os.listdir(dir):
+        fpath = os.path.join(dir,f)
+        if os.path.isfile(fpath):
+            print(f"removing {fpath}")
+            os.unlink(fpath)
 
 @task 
 def build(c, clean=False):
     base_dir = c.config._project_prefix
-    docs_dir = os.path.join(base_dir, 'docs')
+    build_dir = os.path.join(base_dir, 'docs')
+    img_src_dir = os.path.join(base_dir, 'slike')
+    img_build_dir = os.path.join(build_dir, 'slike')
+
     if clean:
-        for f in os.listdir(docs_dir):
-            print(f"removing {f} from {docs_dir}")
-            os.unlink(os.path.join(docs_dir,f))
+        remove_files_from_dir(build_dir)
+        remove_files_from_dir(img_build_dir)
+
+    for f in os.listdir(img_src_dir):
+        fsrc = os.path.join(img_src_dir, f)
+        fdst = os.path.join(img_build_dir, f)
+        print(f"copy {fsrc} -> {fdst}")
+        shutil.copyfile(fsrc, fdst)
+
     with c.cd(base_dir):
-        c.run(f"jupyter nbconvert --to html *.ipynb --output-dir {docs_dir}")
+        c.run(f"jupyter nbconvert --to html *.ipynb --output-dir {build_dir}")
